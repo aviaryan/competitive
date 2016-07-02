64fd963e7ba88fff056ea70ee7230bbcba470307
aviaryan <avi.aryan123@gmail.com>
2016-07-02 20:09:04 +0530

    update script

--- clean.sh
+++ clean.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # deletes all no extension executable files
-find . -type f ! -name "*.*" -delete
+#find . -type f ! -name "*.*" -delete
+#http://stackoverflow.com/questions/856463/how-to-remove-delete-executable-files-aka-files-without-extension-only
+find . -type f -executable ! -name "*.*"
+find . -type f -executable ! -name "*.*" -delete
