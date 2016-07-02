3323e69435e178066406ec09f5c5ec014b7e3e26
aviaryan <avi.aryan123@gmail.com>
2016-07-02 20:31:49 +0530

    july16

--- codechef/july16/chefarc.cpp
+++ codechef/july16/chefarc.cpp
@@ -12,10 +12,16 @@ typedef long long ll;
 
 int n, m;
 int b[100][100];
+int r1[100][100], r2[100][100];
 
-bool possible(int x, int y){
-	if ((x >= 0) && (x<n) && (y >= 0) && (y < m) && b[x][y]==0)
-		return true;
+bool possible(int x, int y, int num){
+	if ((x >= 0) && (x<n) && (y >= 0) && (y < m) && b[x][y]==0){
+		if (num==1)
+			return (r1[x][y] == MODULO);
+		else
+			return (r2[x][y] == MODULO);
+		// return true;
+	}
 	else
 		return false;
 }
@@ -24,7 +30,8 @@ bool possible(int x, int y){
 int main(){
 	int t, i, x, k, j, ans, k1, k2;
 	s(t);
-	int r1[100][100], r2[100][100];
+	queue< pair<int, int> > q;
+	pair <int, int> p1;
 
 	while (t--){
 		// get int
@@ -38,88 +45,63 @@ int main(){
 		}
 		// k1
 		r1[0][0] = 0;
-		for (i=0; i<n; i++){
-			for (j=0; j<m; j++){
-				// all possible jumps
-				if (b[i][j] == 1) 
-					continue;
-				for (k=1; k<=k1; k++){
-					// all possible places when k fixed
-					for (x=0; x<=k; x++){
-						if ( possible(i+x, j+(k-x)) )
-							r1[i+x][j+(k-x)] = min(r1[i+x][j+(k-x)], r1[i][j]+1);
-						if ( possible(i+x, j-(k-x)) )
-							r1[i+x][j-(k-x)] = min(r1[i+x][j-(k-x)], r1[i][j]+1);
-						if ( possible(i-x, j+(k-x)) )
-							r1[i-x][j+(k-x)] = min(r1[i-x][j+(k-x)], r1[i][j]+1);
-						if ( possible(i-x, j-(k-x)) )
-							r1[i-x][j-(k-x)] = min(r1[i-x][j-(k-x)], r1[i][j]+1);
+		q.push(make_pair(0, 0));
+		while (!q.empty()){
+			p1 = q.front();
+			q.pop();
+			i = p1.first;
+			j = p1.second;
+			// all possible K (less thans)
+			for (k=1; k<=k1; k++){
+				// all possible places when k fixed
+				for (x=0; x<=k; x++){
+					if ( possible(i+x, j+(k-x), 1) ){
+						r1[i+x][j+(k-x)] = r1[i][j]+1;
+						q.push(make_pair(i+x, j+(k-x)));
+					}
+					if ( possible(i+x, j-(k-x), 1) ){
+						r1[i+x][j-(k-x)] = r1[i][j]+1;
+						q.push(make_pair(i+x, j-(k-x)));
+					}
+					if ( possible(i-x, j+(k-x), 1) ){
+						r1[i-x][j+(k-x)] = r1[i][j]+1;
+						q.push(make_pair(i-x, j+(k-x)));
+					}
+					if ( possible(i-x, j-(k-x), 1) ){
+						r1[i-x][j-(k-x)] = r1[i][j]+1;
+						q.push(make_pair(i-x, j-(k-x)));
 					}
 				}
 			}
 		}
-		// k1 reverse
-		// r1[0][0] = 0;
-		for (i=n-1; i>=0; i--){
-			for (j=m-1; j>=0; j--){
-				// all possible jumps
-				if (b[i][j] == 1) 
-					continue;
-				for (k=1; k<=k1; k++){
-					// all possible places when k fixed
-					for (x=0; x<=k; x++){
-						if ( possible(i+x, j+(k-x)) )
-							r1[i+x][j+(k-x)] = min(r1[i+x][j+(k-x)], r1[i][j]+1);
-						if ( possible(i+x, j-(k-x)) )
-							r1[i+x][j-(k-x)] = min(r1[i+x][j-(k-x)], r1[i][j]+1);
-						if ( possible(i-x, j+(k-x)) )
-							r1[i-x][j+(k-x)] = min(r1[i-x][j+(k-x)], r1[i][j]+1);
-						if ( possible(i-x, j-(k-x)) )
-							r1[i-x][j-(k-x)] = min(r1[i-x][j-(k-x)], r1[i][j]+1);
-					}
-				}
-			}
-		}
+
 		// k2
 		r2[0][m-1] = 0;
-		for (i=0; i<n; i++){
-			for (j=m-1; j>=0; j--){
-				// all possible jumps
-				if (b[i][j] == 1) 
-					continue;
-				for (k=1; k<=k2; k++){
-					// all possible places when k fixed
-					for (x=0; x<=k; x++){
-						if ( possible(i+x, j+(k-x)) )
-							r2[i+x][j+(k-x)] = min(r2[i+x][j+(k-x)], r2[i][j]+1);
-						if ( possible(i+x, j-(k-x)) )
-							r2[i+x][j-(k-x)] = min(r2[i+x][j-(k-x)], r2[i][j]+1);
-						if ( possible(i-x, j+(k-x)) )
-							r2[i-x][j+(k-x)] = min(r2[i-x][j+(k-x)], r2[i][j]+1);
-						if ( possible(i-x, j-(k-x)) )
-							r2[i-x][j-(k-x)] = min(r2[i-x][j-(k-x)], r2[i][j]+1);
+		q.push(make_pair(0, m-1));
+		while (!q.empty()){
+			p1 = q.front();
+			q.pop();
+			i = p1.first;
+			j = p1.second;
+			// all possible K (less thans)
+			for (k=1; k<=k2; k++){
+				// all possible places when k fixed
+				for (x=0; x<=k; x++){
+					if ( possible(i+x, j+(k-x), 2) ){
+						r2[i+x][j+(k-x)] = r2[i][j]+1;
+						q.push(make_pair(i+x, j+(k-x)));
 					}
-				}
-			}
-		}
-		// k2 reverse
-		// r2[0][m-1] = 0;
-		for (i=n-1; i>=0; i--){
-			for (j=0; j<m; j++){
-				// all possible jumps
-				if (b[i][j] == 1) 
-					continue;
-				for (k=1; k<=k2; k++){
-					// all possible places when k fixed
-					for (x=0; x<=k; x++){
-						if ( possible(i+x, j+(k-x)) )
-							r2[i+x][j+(k-x)] = min(r2[i+x][j+(k-x)], r2[i][j]+1);
-						if ( possible(i+x, j-(k-x)) )
-							r2[i+x][j-(k-x)] = min(r2[i+x][j-(k-x)], r2[i][j]+1);
-						if ( possible(i-x, j+(k-x)) )
-							r2[i-x][j+(k-x)] = min(r2[i-x][j+(k-x)], r2[i][j]+1);
-						if ( possible(i-x, j-(k-x)) )
-							r2[i-x][j-(k-x)] = min(r2[i-x][j-(k-x)], r2[i][j]+1);
+					if ( possible(i+x, j-(k-x), 2) ){
+						r2[i+x][j-(k-x)] = r2[i][j]+1;
+						q.push(make_pair(i+x, j-(k-x)));
+					}
+					if ( possible(i-x, j+(k-x), 2) ){
+						r2[i-x][j+(k-x)] = r2[i][j]+1;
+						q.push(make_pair(i-x, j+(k-x)));
+					}
+					if ( possible(i-x, j-(k-x), 2) ){
+						r2[i-x][j-(k-x)] = r2[i][j]+1;
+						q.push(make_pair(i-x, j-(k-x)));
 					}
 				}
 			}
