a8e8f116eb326f056b7c589b55475654a32d97fe
aviaryan <avi.aryan123@gmail.com>
2016-09-15 15:16:28 +0530

    updated circleq, added some starter code, don't have the motivation to solve

--- codechef/sept16/circleq.cpp
+++ codechef/sept16/circleq.cpp
@@ -12,13 +12,40 @@ typedef long long ll;
 
 // clean circles, remove childs
 // take each circle, calc its influence over all units
-// use lazy propogration for full units
 // now we have unit areas
+struct circle {
+	int x;
+	int y;
+};
+typedef struct circle circle;
+
+bool grid[50000][50000];
+map <int, circle> circles;
+
+// makes a circle
+circle make_circle(int x, int y){
+	circle c;
+	c.x = x;
+	c.y = y;
+	return c;
+}
 
 int main(){
-	int t, i;
-	ios_base::sync_with_stdio(0);
-	cin >> t;
+	int t, i, n, q, x, y, r, x1, y1, x2, y2;
+	s(n); s(q);
+
+	for (i=0; i<n; i++){
+		scanf("%d %d %d", &x, &y, &r);
+		circles.insert(make_pair(r, make_circle(x, y)));
+	}
+
+	// fill unit areas
+	// create super array
+
+	while (q--){
+		scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+		printf("%lf\n", 2.3);
+	}
 
 	return 0;
 }
