1eeff0db6c2b6075dc5f1ec5f5ae69fe1660dada
aviaryan <avi.aryan123@gmail.com>
2016-09-30 16:13:34 +0530

    updated hackerrank matrix_rotation_algo solution

--- hackerrank/matrix-rotation-algo.cpp
+++ hackerrank/matrix-rotation-algo.cpp
@@ -12,29 +12,75 @@ typedef long long ll;
 
 
 int main(){
-	int t, i, n, m, r;
-	ios_base::sync_with_stdio(0);
-	cin >> n >> m >> r;
+	int t, i, n, m, r, j;
+	s(m); s(n); s(r);
 
-	int arr[n][m];
+	int arr[m][n];
 
-	for (i=0; i<n; i++){
-		for (j=0; j<m; j++){
+	for (i=0; i<m; i++)
+		for (j=0; j<n; j++)
 			scanf("%d", &arr[i][j]);
+
+	int sz = min(n, m) / 2;
+	int nb = n, mb = m, length, rots, temp;
+	int is = 0, ie = m-1, js = 0, je = n-1;
+	deque <int> v;
+	deque <int>::iterator vi;
+
+	while (sz--){
+		length = 2*nb + 2*mb - 4;
+		rots = r % length;
+
+		v.clear();
+		// journey
+		for (i = is; i <= ie; i++){
+			v.push_back(arr[i][js]);
 		}
+		for (j = js+1; j<=je; j++){
+			v.push_back(arr[ie][j]);
+		}
+		for (i = ie-1; i>=is; i--){
+			v.push_back(arr[i][je]);
+		}
+		for (j = je-1; j>js; j--){
+			v.push_back(arr[is][j]);
+		}
+		// reorder
+		while (rots--){
+			temp = v.back();
+			v.pop_back();
+			v.push_front(temp);
+		}
+
+		vi = v.begin();
+		// journey update
+		for (i = is; i <= ie; i++){
+			arr[i][js] = *vi;
+			vi++;
+		}
+		for (j = js+1; j<=je; j++){
+			arr[ie][j] = *vi;
+			vi++;
+		}
+		for (i = ie-1; i>=is; i--){
+			arr[i][je] = *vi;
+			vi++;
+		}
+		for (j = je-1; j>js; j--){
+			arr[is][j] = *vi;
+			vi++;
+		}
+
+		is++; ie--; js++; je--;
+		nb -= 2; mb -= 2;
 	}
 
-	int cpy[n][m], szs[min(n,m)/2];
-
-	int nb = n, mb = m, i=0;
-	while (nb * mb > 0){
-		szs[i] = nb * mb;
-		i++;
-		nb -= 2;
-		mb -= 2;
+	// print rotated
+	for (i=0; i<m; i++){
+		for (j=0; j<n; j++)
+			printf("%d ", arr[i][j]);
+		printf("\n");
 	}
-
-	
 
 	return 0;
 }
