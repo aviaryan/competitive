ca07e8941d0bdc242b44884fd94bc2766a317819
Avi Aryan <avi.aryan123@gmail.com>
2024-06-21 00:35:16 +0530

    codes

--- leetcode/medium/longest-substring-without-repeating-characters.ts
+++ leetcode/medium/longest-substring-without-repeating-characters.ts
@@ -1,14 +1,18 @@
 function lengthOfLongestSubstring(s: string): number {
   type CharMap = { [key: number]: boolean };
   let answer = 0;
-  let j;
+  let j, lastLeftJ = 0;
+  let charMap: CharMap = {};
 
   for (let i = 0; i < s.length; i++) {
-    let charMap: CharMap = {};
-    for (j = i; j < s.length; j++) {
+    for (j = Math.max(i, lastLeftJ); j < s.length; j++) {
       const code = s.charCodeAt(j);
       if (charMap[code]) {
         answer = Math.max(answer, j - i);
+        // update the 'i' and keep going from current "j"
+        // since we know non repeating string till before that
+        charMap[s.charCodeAt(i)] = false;
+        lastLeftJ = j;
         break;
       } else {
         charMap[code] = true;
@@ -16,8 +20,9 @@ function lengthOfLongestSubstring(s: string): number {
     }
     if (j === s.length) {
       answer = Math.max(answer, j - i);
+      lastLeftJ = 0; // reset it
     }
   }
 
   return answer;
-}
+};
